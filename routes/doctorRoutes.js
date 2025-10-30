64cd5c0a848844124fead0e1316c756fb6bd97b2
dillona25 <Dillyb00by49@gmail.com>
2025-10-29 17:16:19 -0700

    Fix: Adjusted clinic req body to meet frontend

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -2,10 +2,11 @@ import express from "express";
 import { createDoctor, getUsersDoctor } from "../controllers/doctorController.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
+
 const router = express.Router();
 
 // POST /api/doctors
 router.post("/", verifyToken, createDoctor);
-router.get("/", verifyToken, getUsersDoctor)
+router.get("/", verifyToken, getUsersDoctor);
 
 export default router;
