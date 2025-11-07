23827da69606817074db45d5d755264ad43639d1
dillona25 <Dillyb00by49@gmail.com>
2025-11-06 20:17:39 -0800

    Feat: Added Endpoints to Update OR Delete Apts

--- controllers/appointmentsController.js
+++ controllers/appointmentsController.js
@@ -16,7 +16,8 @@ export const createAppointment = async (req, res) => {
     !doctor_id ||
     !doctor_name ||
     !appointment_title ||
-    !appointment_date || !appointment_time
+    !appointment_date ||
+    !appointment_time
   ) {
     return res.status(400).json({ error: "Missing required fields" });
   }
@@ -50,7 +51,7 @@ export const createAppointment = async (req, res) => {
 };
 
 export const getUsersAppointments = async (req, res) => {
-    try {
+  try {
     const query = `SELECT * FROM appointments
     WHERE user_id = $1
     ORDER BY appointment_date ASC, appointment_time ASC`;
@@ -62,4 +63,67 @@ export const getUsersAppointments = async (req, res) => {
     console.error("Error fetching doctors:", error);
     res.status(500).json({ error: "Server error" });
   }
-}
+};
+
+export const deleteAppointment = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = `DELETE FROM appointments WHERE id = $1`;
+    const values = [id];
+    const result = await pool.query(query, values);
+
+    // If no rows were deleted, the appointment didn't exist
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Appointment not found" });
+    }
+
+    res.status(200).send();
+  } catch (error) {
+    console.error("Error deleting appointment:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const updateAppointment = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+   // If no fields provided, no update to perform
+  if (Object.keys(updates).length === 0) {
+    return res.status(400).json({ error: "No fields provided for update" });
+  }
+
+  try {
+    // Building our SET..
+    const setClauses = [];
+    const values = [];
+    let index = 1;
+
+    // Updates is a KV pair and we push the index to the key and the value is the updated value
+    for (const [key, value] of Object.entries(updates)) {
+      setClauses.push(`${key} = $${index}`);
+      values.push(value);
+      index++;
+    }
+    values.push(id);
+
+    const query = `
+      UPDATE appointments
+      SET ${setClauses.join(", ")}
+      WHERE id = $${index}
+      RETURNING *;
+    `;
+
+    const result = await pool.query(query, values);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Appointment not found" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.error("Error updating appointment:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
