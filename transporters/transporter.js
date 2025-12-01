3ac541820108911565287632cab6b7778cf5ce90
dillona25 <Dillyb00by49@gmail.com>
2025-11-30 16:37:25 -0800

    Added twilio file for SMS messgaing. Still work in progress.

--- transporters/transporter.js
+++ transporters/transporter.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
-
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
+const twilio = require("twilio"); 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
@@ -15,8 +11,6 @@ async function createMessage() {
     from: "+15037651409",
     to: "+13609471388",
   });
-
-  console.log(message.body);
 }
 
 createMessage();
